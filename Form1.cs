4f8f6ea101db1c30455a8d9b4429e9f182c27120
STUDENTS-STEM-25 <STUDENTS-STEM-25@DESKTOP-SCKI0LB>
2023-03-09 12:42:18 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextovRedaktor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Текстови файлове|*.txt|Word файлове|*.doc, *.docx|All files|*.*";
+            saveFileDialog1.FilterIndex = 1;
+            saveFileDialog1.ShowDialog();
+            richTextBox1.SaveFile(saveFileDialog1.FileName, RichTextBoxStreamType.RichText);
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Текстови файлове|*.txt|Word файлове|*.doc, *.docx|All files|*.*";
+            openFileDialog1.ShowDialog();
+            richTextBox1.LoadFile(openFileDialog1.FileName, RichTextBoxStreamType.RichText);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Modified)
+            {
+                DialogResult dialogResult = MessageBox.Show("Do you want to save", "Accept", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    saveAsToolStripMenuItem_Click(sender, e);
+                }
+                if (dialogResult == DialogResult.Cancel)
+                {
+                    this.Close();
+                }
+            }
+        }
+
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fontDialog1.ShowEffects = true;
+            fontDialog1.ShowColor = true;
+            fontDialog1.ShowDialog();
+            richTextBox1.SelectionFont = fontDialog1.Font;
+            richTextBox1.SelectionColor = fontDialog1.Color;
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Modified)
+            {
+                DialogResult dialogResult = MessageBox.Show("Do you want to save", "Accept", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    saveAsToolStripMenuItem_Click(sender, e);
+                }
+                if (dialogResult == DialogResult.No)
+                {
+                    richTextBox1.Clear();
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (richTextBox1.Modified)
+            {
+                DialogResult dialogResult = MessageBox.Show("Do you want to save", "Accept", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    saveAsToolStripMenuItem_Click(sender, e);
+                }
+                if (dialogResult == DialogResult.Cancel)
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+    }
+}
